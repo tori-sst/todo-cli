ea7185dbe99ce18b1292d7b48bea88110df28c7a
Vik <viktoriastermer@gmail.com>
2026-09-24 09:26:48 +0500

    init flags

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,1 +1,28 @@
-package todo
+package main
+
+import "flag"
+
+func main(){
+	addCmd := flag.String("add", "", "Add a new task (name it)")
+	listCmd := flag.Bool("list", false, "Show task list")
+	deleteCmd := flag.Int("delete", 0, "Delete task by its ID")
+
+	flag.Parse()
+
+	if *addCmd != "" {
+		println("Add task:", *addCmd)
+		return
+	}
+
+	if *listCmd {
+		println("Task list")
+		return
+	}
+
+	if *deleteCmd != 0 {
+		println("Delete task with ID: ", *deleteCmd)
+		return
+	}
+
+	println("Tip: go run ./cmd/todo -add \"Taskname\" | -list | -delete ID")
+}
